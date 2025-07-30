9bb4f0a49ef829aa65a5de94416d1413aed3300d
Yukimura-Hanzo <t.personal.business@gmail.com>
2025-07-30 21:52:32 +0100

    build: @next/mdx package, and related packages, are used to configure Next.js so it can process markdown & MDX

---
+++ mdx-components.tsx
@@ -0,0 +1,8 @@
+//? Define Global MDX Components | @next/mdx with App Router
+import type { MDXComponents } from "mdx/types";
+
+const components: MDXComponents = {};
+
+export function useMDXComponents(): MDXComponents {
+  return components;
+}
