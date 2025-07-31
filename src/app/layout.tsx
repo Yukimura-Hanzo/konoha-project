e2a14695948f7f2367c3fc7264b91d90f533b8a4
Yukimura-Hanzo <t.personal.business@gmail.com>
2025-07-31 18:52:24 +0100

    build: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/css/globals.css";
 import "@/styles/scss/globals.scss";
@@ -50,6 +51,7 @@ export default function RootLayout({
           <Footer />
         </div>
         <SpeedInsights/>
+        <Analytics />
       </body>
     </html>
   );
